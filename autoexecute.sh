a0ad9b09e7bce7afd35eca6ff59f3ac89d49ae70
Som23Git <somesh.rokz@gmail.com>
2022-12-25 17:37:19 +0530

    Updated all the necessay script files to be executed

--- autoexecute.sh
+++ autoexecute.sh
@@ -59,6 +59,61 @@ echo "* Granting Permissions to the service account"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member serviceAccount:my-tts-sa@${PROJECT_ID}.iam.gserviceaccount.com --role roles/serviceusage.serviceUsageConsumer
 
 # Create credentials that your Python code will use to login as your new service account. Create and save these credentials as a ~/key.json JSON file by using the following command
+echo "* Creating credentials and save it as a key.json file in the home directory"
+gcloud iam service-accounts keys create ~/key.json --iam-account my-tts-sa@${PROJECT_ID}.iam.gserviceaccount.com
 
+# Finally, set the GOOGLE_APPLICATION_CREDENTIALS environment variable, which is used by the Speech-to-Text client library, to find your credentials.
+echo "* Exporting GOOGLE_APPLICATION_CREDENTIALS"
+export GOOGLE_APPLICATION_CREDENTIALS=~/key.json
 
+# Checking whether the Python3 is installed
+echo "-------------------------------------"
+echo " "
+echo "* Checking whether the Python is installed"
+# Check if Python 3 is installed
+if ! command -v python3 > /dev/null; then
+    # Install Python 3
+    brew install python
+else
+    echo "Python3 is installed already"
+    python3 -V
+fi
 
+# Check if pip3 is installed
+if ! command -v pip3 > /dev/null; then
+    # Install pip3
+    python3 -m ensurepip --upgrade
+else
+    echo "pip3 is installed already"
+    pip3 -V
+fi
+
+# Install Python Client Library
+echo "-------------------------------------"
+echo "INSTALLATION OF PYTHON CLIENT LIBRARY"
+echo "-------------------------------------"
+pip3 install --user --upgrade google-cloud-texttospeech
+
+# All installation is completed
+
+echo "------------------------------------------"
+echo "XXX ALL PACKAGE INSTALLATION COMPLETED XXX"
+echo "------------------------------------------"
+
+# Prompt the user for input
+read -p "Enter the text to be converted to speech: " input
+
+# Write the input to a file
+echo "$input" > xyz_file.txt
+echo " "
+echo "The text is added successfully to the file xyz_file.txt"
+
+# Let's process the text file to the gcloud API
+echo "-----------------------------"
+echo "CONVERTING THE TEXT TO SPEECH"
+echo "-----------------------------"
+python3 synthesize_file.py --text xyz_file.txt
+
+echo "-----------------------------"
+echo "OUTPUT.MP3 IS READY!!!"
+echo "-----------------------------"
